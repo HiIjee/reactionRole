433a49c11018702e8940bc5222937a4ccdabc4ec
HiIjee <121424622+HiIjee@users.noreply.github.com>
2022-12-29 18:35:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const discord = require('discord.js');
-const ReactionRole = require('./ROLES/ReactionRole');
+const ReactionRole = require('./ReactionRole');
 const client= new discord.Client();
 const config= require('./config.json')
 const { execute, collector, member, embed,button, data,SlashCommandBuilder, PermissionsBitField, ButtonStyle, ActionRowBuilder, ButtonBuilder, EmbedBuilder }= require("./ROLES/ReactionRole.js")
