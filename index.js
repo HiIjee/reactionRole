4b881e4d8bef2e70a4118314a46938a2208d2100
HiIjee <121424622+HiIjee@users.noreply.github.com>
2022-12-29 18:32:43 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const discord = require('discord.js');
+const ReactionRole = require('./ROLES/ReactionRole');
+const client= new discord.Client();
+const config= require('./config.json')
+const { execute, collector, member, embed,button, data,SlashCommandBuilder, PermissionsBitField, ButtonStyle, ActionRowBuilder, ButtonBuilder, EmbedBuilder }= require("./ROLES/ReactionRole.js")
+
+
+client.on('ready', () =>{
+    console.log('Im here')
+})
+
+client.login(config.TOKEN)
+
+execute();
+
+
